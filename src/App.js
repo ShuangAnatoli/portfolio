df8caeafa51247042f716dce230cb1f40eb1bbc1
Swann <swannsu@gmail.com>
2023-02-08 20:46:15 +0630

    Files added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import About from "./components/Navbar";
 import About from "./components/About";
+import About from "./components/Projects";
 
 export default function App() {
   return (
     <main>
+      <Navbar />
       <About />
+      <Projects />
       
     </main>
   );
